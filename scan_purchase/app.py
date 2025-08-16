1b01bc92ca25aff123b720c657609c81b257d280
Lovetiwari-art <Lovetiwari@gmipl.in>
2025-08-16 15:12:44 +0530

    Update app.py

--- scan_purchase/app.py
+++ scan_purchase/app.py
@@ -202,7 +202,17 @@ def submit_grn():
 # --- START OF CHANGES ---
 # This is the only part that has been modified.
 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
 
+
 # --- END OF CHANGES ---
+
